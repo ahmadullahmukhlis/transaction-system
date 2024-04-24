47531f24f440c7912b29dd897da404cfae4daf24
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-04-24 20:29:38 +0430

    the problem of the dashboard is fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -70,20 +70,121 @@
 
             <!--after-->
             <div class="row">
-                <div class="table-responsive">
-                    <table class="table table-dark">
-                        <thead>
-                            <tr>
-                                <th>name</th>
-                                <th>father name</th>
-                                <th>contact nunber</th>
-                                <th>address</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="card-title">Online Store Visitors</h3>
+                                    <a href="javascript:void(0);">View Report</a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="d-flex">
+                                    <p class="d-flex flex-column">
+                                        <span class="text-bold text-lg">820</span>
+                                        <span>Visitors Over Time</span>
+                                    </p>
+                                    <p class="ml-auto d-flex flex-column text-right">
+                                        <span class="text-success">
+                                            <i class="fas fa-arrow-up"></i> 12.5%
+                                        </span>
+                                        <span class="text-muted">Since last week</span>
+                                    </p>
+                                </div>
 
-                        </tbody>
-                    </table>
+                                <div class="position-relative mb-4">
+                                    <div class="chartjs-size-monitor">
+                                        <div class="chartjs-size-monitor-expand">
+                                            <div class=""></div>
+                                        </div>
+                                        <div class="chartjs-size-monitor-shrink">
+                                            <div class=""></div>
+                                        </div>
+                                    </div>
+                                    <canvas id="visitors-chart" height="200" width="770"
+                                        style="display: block; width: 770px; height: 200px;"
+                                        class="chartjs-render-monitor"></canvas>
+                                </div>
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="mr-2">
+                                        <i class="fas fa-square text-primary"></i> This Week
+                                    </span>
+                                    <span>
+                                        <i class="fas fa-square text-gray"></i> Last Week
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <h3 class="card-title">Products</h3>
+                                <div class="card-tools">
+                                    <a href="#" class="btn btn-tool btn-sm">
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                    <a href="#" class="btn btn-tool btn-sm">
+                                        <i class="fas fa-bars"></i>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="card-title">Sales</h3>
+                                    <a href="javascript:void(0);">View Report</a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="d-flex">
+                                    <p class="d-flex flex-column">
+                                        <span class="text-bold text-lg">$18,230.00</span>
+                                        <span>Sales Over Time</span>
+                                    </p>
+                                    <p class="ml-auto d-flex flex-column text-right">
+                                        <span class="text-success">
+                                            <i class="fas fa-arrow-up"></i> 33.1%
+                                        </span>
+                                        <span class="text-muted">Since last month</span>
+                                    </p>
+                                </div>
+
+                                <div class="position-relative mb-4">
+                                    <div class="chartjs-size-monitor">
+                                        <div class="chartjs-size-monitor-expand">
+                                            <div class=""></div>
+                                        </div>
+                                        <div class="chartjs-size-monitor-shrink">
+                                            <div class=""></div>
+                                        </div>
+                                    </div>
+                                    <canvas id="sales-chart" height="200" width="770"
+                                        style="display: block; width: 770px; height: 200px;"
+                                        class="chartjs-render-monitor"></canvas>
+                                </div>
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="mr-2">
+                                        <i class="fas fa-square text-primary"></i> This year
+                                    </span>
+                                    <span>
+                                        <i class="fas fa-square text-gray"></i> Last year
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div id="chartContainer" class="text-white  bf-black"
+                                style="height: 370px; width: 100%;"></div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
