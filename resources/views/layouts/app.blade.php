cfacfda2d0647b5a82a50d140ec86781f33633f1
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-04-24 19:14:50 +0430

    the public function and error is fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -259,12 +259,14 @@
 
     <!-- PAGE PLUGINS -->
     <!-- jQuery Mapael -->
-    <script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+    <script src="{{ asset('plugins/jquery-mousewheel/jquery.mousewheel.js') }}"></script>
     <script src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
     <script src="{{ asset('plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
     <script src="{{ asset('plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
     <!-- ChartJS -->
-    <script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('plugins/chart/chart.js') }}"></script>
+    <script src="{{ asset('plugins/chart/Chart.min.js') }}"></script>
+
 </body>
 
 </html>
