1c6017645fbdf26e0898ec5d7acfa994788f2995
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-04-28 18:51:34 +0430

    the problem fo the function is fixed

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+
+    dashboae
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\get;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,11 +13,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/setting', Dashboard::class)->middleware(['auth', 'verified'])->name('setting');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
